f1f3b53896f712066ff723b49fae191ac84479a6
kgk1001 <kgk1001@outloo.com>
2022-07-22 00:15:29 +0530

    defined the enemy and enemy movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # initialize pygame
 pygame.init()
@@ -17,9 +18,20 @@ plyX = 368
 plyY = 536
 plyChg = 0
 
+# enemy
+enIm = pygame.image.load("alien.png")
+enX = 0
+enY = 0
+enChgX = 0.2
+enChgY = 0.25
+
 
 def player(x, y):
     screen.blit(plyIm, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enIm, (x, y))
 
 
 # game loop
@@ -37,15 +49,30 @@ while running:
         # movement
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                plyChg = -0.1
+                plyChg = -0.2
                 print("left")
             if event.key == pygame.K_RIGHT:
-                plyChg = 0.1
+                plyChg = 0.2
                 print("right")
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                 plyChg = 0
 
     plyX += plyChg
+    # set the player boundary
+    if plyX < 0:
+        plyX = 0
+    elif plyX > 736:
+        plyX = 736
+
+    enX += enChgX
+    if enX < 0:
+        enChgX = 0.2
+        enY += 32
+    elif enX > 736:
+        enChgX = -0.2
+        enY += 32
+
     player(plyX, plyY)
+    enemy(enX, enY)
     pygame.display.update()

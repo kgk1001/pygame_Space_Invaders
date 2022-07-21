d93c3b50855203481d6b1a8fa290e894ec372d37
kgk1001 <kgk1001@outloo.com>
2022-07-21 20:39:37 +0530

    change icon and the title also set a bachground colour and set update function

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import pygame
 # initialize pygame
 pygame.init()
 
-#create the screen
-screen = pygame.display.set_mode((800,600))
+# create the screen
+screen = pygame.display.set_mode((800, 600))
 
-#game loop
+# titlle and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("ufo.png")  # load the image
+pygame.display.set_icon(icon)  # place the icon
+
+# game loop
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             running = False
+
+        screen.fill((80, 80, 150))
+        pygame.display.update()

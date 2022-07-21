75cfcfc8d5ff3e41f92b97521567eb942a6b1365
kgk1001 <kgk1001@outloo.com>
2022-07-21 23:30:35 +0530

    wrote player movement

--- main.py
+++ main.py
@@ -6,28 +6,46 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
-# titlle and icon
+# title and icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("ufo.png")  # load the image
 pygame.display.set_icon(icon)  # place the icon
 
 # player
 plyIm = pygame.image.load("spaceship.png")
-plyX =368
+plyX = 368
 plyY = 536
+plyChg = 0
 
 
-def player():
-    screen.blit(plyIm, (plyX, plyY))
+def player(x, y):
+    screen.blit(plyIm, (x, y))
 
 
 # game loop
 running = True
 while running:
     screen.fill((80, 80, 150))
+
+    # check all the events
     for event in pygame.event.get():
+
+        # function to quit the game
         if event.type == pygame.QUIT:
             running = False
 
-        player()
-        pygame.display.update()
+        # movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                plyChg = -0.1
+                print("left")
+            if event.key == pygame.K_RIGHT:
+                plyChg = 0.1
+                print("right")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                plyChg = 0
+
+    plyX += plyChg
+    player(plyX, plyY)
+    pygame.display.update()
